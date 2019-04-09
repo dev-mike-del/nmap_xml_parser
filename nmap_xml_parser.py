1281429b8fc5c64749f13e275cb8e9abae6ec57d
Your Name <you@example.com>
2019-04-08 19:12:12 -0700

    Add comments

--- nmap_xml_parser.py
+++ nmap_xml_parser.py
@@ -1,11 +1,30 @@
 #! user/bin/env
 
+
+# This Python3 script parses an xml file created by the Nmap 
+# command "nmap -A -T4 -oX path_to/xml_file_name.xml your_research_target.com"
+#
+# Substitute "path_to/xml_file_name.xml" and "your_research_target.com" with the 
+# correct information.
+#
+# You can use this script to turn an nmap xml file (specified above) into a python dictionary 
+# or you can use it to create a python dictionary of the differences between two different 
+# Nmap xml file. 
+#
+# Once you have an xml file you can run this file in the interactive mode 
+# (python3 -i this_script_name.py). Then, use your xml path with the 'get_root(path)'
+# function and the result of that with the 'get_dict(root)' function.
+
+
+# xml.etree.ElementTree — The ElementTree XML API
+# https://docs.python.org/3/library/xml.etree.elementtree.html
 import xml.etree.ElementTree as ET
 
-
+# ElementTree function to parse xml and create an xml.etree.ElementTree.Element class instance
 def get_root(path):
 	return ET.parse(path).getroot()
 
+# Parses xml.etree.ElementTree.Element class instance into a python dictionary
 def get_dict(root):
 	d = {root.tag: root.attrib,}
 	
@@ -18,24 +37,30 @@ def get_dict(root):
 
 	return d
 
+# Creates a dictionary of the difference between two dictionaries (created from this script)
 def get_diff(d1, d2):
 	d = {}
-	for k,v in d1.items():
-		if d1[k] != d2[k]:
-			if d.__contains__(k):
-				d[k]['d1'] = d1[k]
-				d[k]['d2'] = d2[k]
+	for k1,v in d1.items():
+		if d1[k1] != d2[k1]:
+			if d.__contains__(k1):
+				if d1[k1].__len__() > 1:
+					for k2,v in d1[k1].items():
+						if d1[k1][k2] != d2[k1][k2]:
+							d[k1]['d1'] = d1[k1][k2]
+							d[k1]['d2'] = d2[k1][k2]
+				else:
+					d[k1]['d1'] = d1[k1]
+					d[k1]['d2'] = d2[k1]
 			else:
-				d[k] = {'d1': d1[k]}
-				d[k]['d2'] = d2[k]
+				if d1[k1].__contains__(1):
+					for k2,v in d1[k1].items():
+						d[k1] = {'d1': d1[k1][k2]}
+						d[k1]['d2'] = d2[k1][k2]
+				else:
+					for k2,v in d1[k1].items():
+						d[k1] = {'d1': {d1[k1][k2]}}
+						d[k1]['d2'] = d2[k1][k2]
+					d[k1] = {'d1': d1[k1]}
+					d[k1]['d2'] = d2[k1]
 
 	return d
-
-
-r1 = get_root('worldstoryboard.xml')
-r2 = get_root('worldstoryboard2.xml')
-
-d1 = get_dict(r1)
-d2 = get_dict(r2)
-diff = get_diff(d1, d2)
-
